e4604b2b564e6bd31fcb590fd079cf9290c078d0
Kirush29 <it24104106@my.sliit.lk>
2024-11-06 15:31:06 +0530

    Add files via upload

---
+++ IT24104106Lab3Q2.java
@@ -0,0 +1,17 @@
+public class IT24104106Lab3Q2{
+  public static void main (String[]args){
+   int monthlysalary=50000;
+   int OThours=5;
+   int OThourlyrate=1000;
+   double totalsalary=(monthlysalary + (OThours*OThourlyrate));
+      System.out.println("Enter the monthly salary:" +monthlysalary);
+    System.out.println("Enter the number of OT hours:" +OThours);
+    System.out.println();
+    System.out.println(" The total salary including OT is :"+ totalsalary);
+
+    }
+}
+    
+
+
+   
